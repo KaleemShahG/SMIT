6b0e3cfa07ee1e25cf507b5d3e6444f82b6aeafa
KaleemShahG <shahkaleem269@gmail.com>
2025-12-12 10:59:34 +0500

    Auto update from push.bat

---
+++ JAVA-SCRIPT/Class 5/app.js
@@ -0,0 +1,13 @@
+console.log("kaleem");
+
+// Array
+
+var students = ["aslam","mohsin","farhan","imtiaz"];
+console.log(students);
+console.log(students[1]);
+
+students[4]="usman";
+
+console.log(students);
+
+
