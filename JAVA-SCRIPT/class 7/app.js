3f313182e28d13f0c81714cae661ebb6bdb2be32
KaleemShahG <shahkaleem269@gmail.com>
2025-12-21 10:53:19 +0500

    Auto update from push.bat

---
+++ JAVA-SCRIPT/class 7/app.js
@@ -0,0 +1,34 @@
+// ============toLowerCase==============
+let name = "SYED KALEEM SHAH";
+let lowerName = name.toLowerCase();
+console.log(lowerName);
+
+// =============toUpperCase==============
+let name2 = "syed kaleem shah";
+let UpperName2 = name2.toUpperCase();
+console.log(UpperName2);
+
+//================Slice=================
+var text = "sAylaNi"
+var firstChar = text.slice(0,1).toUpperCase();
+var lastChar = text.slice(1).toLowerCase()
+console.log(firstChar + lastChar);
+
+
+// ==========indexOf & LastIndexOf==========
+var text = "yes may be possible and may not be"
+console.log(text.indexOf("may"));
+console.log(text.lastIndexOf("may"));
+
+// =============fOR lOOPS=================
+for (var i=1; i<=10; i++){
+    console.log(i);
+    }
+
+//================CharAT===================
+let myname = "Kaleem";
+let firstChar = myname.charAt(0);
+console.log(firstChar);
+
+// ===============Replace=================
+
