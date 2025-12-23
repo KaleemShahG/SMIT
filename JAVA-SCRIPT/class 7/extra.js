1e8a5d64109dafc626544de0371a6d7f6513c1b9
KaleemShahG <shahkaleem269@gmail.com>
2025-12-23 22:52:22 +0500

    Auto update from push.bat

--- JAVA-SCRIPT/class 7/extra.js
+++ JAVA-SCRIPT/class 7/extra.js
@@ -12,3 +12,7 @@ console.log(myfirstChar);
 let name2 = "syed kaleem shah";
 let UpperName2 = name2.toUpperCase();
 console.log(UpperName2);
+// ============toLowerCase==============
+let name = "SYED KALEEM SHAH";
+let lowerName = name.toLowerCase();
+console.log(lowerName);
