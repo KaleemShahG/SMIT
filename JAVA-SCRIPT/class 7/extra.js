742609bb3e0c96760df30493d3fc3bfe84cb0203
KaleemShahG <shahkaleem269@gmail.com>
2025-12-23 22:51:59 +0500

    Auto update from push.bat

--- JAVA-SCRIPT/class 7/extra.js
+++ JAVA-SCRIPT/class 7/extra.js
@@ -8,12 +8,7 @@ let myname = "Kaleem";
 let myfirstChar = myname.charAt(0);
 console.log(myfirstChar);
 
-// ===============Replace=================
-let sentence = "I like JavaScript";
-let newSentence = sentence.replace("JavaScript", "JS");
-console.log(newSentence);
-
-//================CharAT===================
-let myname = "Kaleem";
-let myfirstChar = myname.charAt(0);
-console.log(myfirstChar);
+// =============toUpperCase==============
+let name2 = "syed kaleem shah";
+let UpperName2 = name2.toUpperCase();
+console.log(UpperName2);
