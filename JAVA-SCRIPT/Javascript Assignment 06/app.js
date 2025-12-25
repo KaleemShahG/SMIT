897b0b1fbb89cf6f21b30985b228989f34549c35
KaleemShahG <shahkaleem269@gmail.com>
2025-12-25 13:42:41 +0500

    Auto update from push.bat

---
+++ JAVA-SCRIPT/Javascript Assignment 06/app.js
@@ -0,0 +1,9 @@
+// var firstName = prompt("Enter your first name:");
+// var lastName = prompt("Enter your last name:");
+
+// var fullName = firstName + " " + lastName;
+
+// alert("Hello " + fullName + "!");
+
+
+
