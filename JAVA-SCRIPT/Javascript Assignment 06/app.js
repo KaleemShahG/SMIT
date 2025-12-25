913f46f4c1c1f8595f9c5122ed78acb4d06549e7
KaleemShahG <shahkaleem269@gmail.com>
2025-12-25 13:47:15 +0500

    Auto update from push.bat

--- JAVA-SCRIPT/Javascript Assignment 06/app.js
+++ JAVA-SCRIPT/Javascript Assignment 06/app.js
@@ -1,21 +1,18 @@
 // var firstName = prompt("Enter your first name:");
 // var lastName = prompt("Enter your last name:");
-
 // var fullName = firstName + " " + lastName;
-
 // alert("Hello " + fullName + "!");
 
 
 // var mobileModel = prompt("Enter your favorite mobile phone model:");
-
 // var length = mobileModel.length;
-
 // alert("Length of your input is: " + length);
 
 
 // var word = "Pakistani";
-
 // var index = word.indexOf("n");
-
 // document.write("The index of letter 'n' in the word Pakistani is: " + index);
 
+// var text = "Hello World";
+// var lastIndex = text.lastIndexOf("l");
+// document.write("The last index of letter 'l' in the word 'Hello World' is: " + lastIndex);
