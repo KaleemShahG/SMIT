4df0207a1de009c2c29460259a68b5f4f4ddca4a
KaleemShahG <shahkaleem269@gmail.com>
2025-12-25 13:42:54 +0500

    Auto update from push.bat

--- JAVA-SCRIPT/Javascript Assignment 06/app.js
+++ JAVA-SCRIPT/Javascript Assignment 06/app.js
@@ -6,4 +6,8 @@
 // alert("Hello " + fullName + "!");
 
 
+// var mobileModel = prompt("Enter your favorite mobile phone model:");
 
+// var length = mobileModel.length;
+
+// alert("Length of your input is: " + length);
