a3c33653876aa6bfab524627e1f23a6a950c6e90
KaleemShahG <shahkaleem269@gmail.com>
2025-12-27 23:06:50 +0500

    Auto update from push.bat

--- JAVA-SCRIPT/Javascript Assignment 07/app.js
+++ JAVA-SCRIPT/Javascript Assignment 07/app.js
@@ -14,9 +14,39 @@
 // document.write("Ceil value: " + Math.ceil(num));
 
 
-let num = parseFloat(prompt("Enter a number"));
+// let num = parseFloat(prompt("Enter a number"));
+// let absValue = Math.abs(num);
+// document.write("Number: " + num + "<br>");
+// document.write("Absolute value: " + absValue);
 
-let absValue = Math.abs(num);
 
-document.write("Number: " + num + "<br>");
-document.write("Absolute value: " + absValue);
+// let dice = Math.floor(Math.random() * 6) + 1;
+// document.write("Dice Value: " + dice);
+
+
+// let coin = Math.floor(Math.random() * 2);
+// if (coin === 0) {
+//   document.write("Coin Value: Heads");
+// } else {
+//   document.write("Coin Value: Tails");
+// }
+
+
+// let randomNum = Math.floor(Math.random() * 100) + 1;
+// document.write("Random Number: " + randomNum);
+
+
+// let weightInput = prompt("Enter your weight");
+// let weight = parseFloat(weightInput);
+// document.write("Your weight is: " + weight + " kgs");
+
+
+// let secretNumber = Math.floor(Math.random() * 10) + 1;
+// let userGuess = parseInt(prompt("Guess the secret number (1 to 10)"));
+// if (userGuess === secretNumber) {
+//   document.write("Congratulations! You guessed the correct number.");
+// } else {
+//   document.write("Sorry! The secret number was " + secretNumber);
+// }
+
+
