ff8732f48e70cdf9f46dc05da2709517caa2a3d6
KaleemShahG <shahkaleem269@gmail.com>
2025-12-27 23:03:09 +0500

    Auto update from push.bat

--- JAVA-SCRIPT/Javascript Assignment 07/app.js
+++ JAVA-SCRIPT/Javascript Assignment 07/app.js
@@ -1,6 +1,14 @@
-let num = parseFloat(prompt("Enter a positive number"));
+// let num = parseFloat(prompt("Enter a positive number"));
 
-console.log("Number: " + num);
-console.log("Round off value: " + Math.round(num));
-console.log("Floor value: " + Math.floor(num));
-console.log("Ceil value: " + Math.ceil(num));
+// console.log("Number: " + num);
+// console.log("Round off value: " + Math.round(num));
+// console.log("Floor value: " + Math.floor(num));
+// console.log("Ceil value: " + Math.ceil(num));
+
+
+let num = parseFloat(prompt("Enter a negative floating point number"));
+
+document.write("Number: " + num + "<br>");
+document.write("Round off value: " + Math.round(num) + "<br>");
+document.write("Floor value: " + Math.floor(num) + "<br>");
+document.write("Ceil value: " + Math.ceil(num));
