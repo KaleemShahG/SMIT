b0c83b826856f958757eb1a917e9452cc5bd22aa
KaleemShahG <shahkaleem269@gmail.com>
2025-12-27 23:02:27 +0500

    Auto update from push.bat

---
+++ JAVA-SCRIPT/Javascript Assignment 07/app.js
@@ -0,0 +1,6 @@
+let num = parseFloat(prompt("Enter a positive number"));
+
+console.log("Number: " + num);
+console.log("Round off value: " + Math.round(num));
+console.log("Floor value: " + Math.floor(num));
+console.log("Ceil value: " + Math.ceil(num));
