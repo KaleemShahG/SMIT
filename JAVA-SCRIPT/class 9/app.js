2f3fface81d910efe12bdb8f18a0511b66f988b9
KaleemShahG <shahkaleem269@gmail.com>
2026-01-02 10:25:08 +0500

    Auto update from push.bat

---
+++ JAVA-SCRIPT/class 9/app.js
@@ -0,0 +1,13 @@
+// chapter 35-to-40
+
+var now = new Date();
+var year = now.getFullYear();
+var month = now.getMonth();
+var date = now.getDate();
+var day = now.getDay();
+var hours = now.getHours();
+var minutes = now.getMinutes();
+var seconds = now.getSeconds();
+
+console.log(hours+ ":" +minutes+":"+seconds);
+console.log( date +":"+ month +":"+ year )
