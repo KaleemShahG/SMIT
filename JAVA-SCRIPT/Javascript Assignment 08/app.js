6eb97a87b3b16b74858cf9d8c25c6ab5f9de103a
KaleemShahG <shahkaleem269@gmail.com>
2026-01-03 23:36:26 +0500

    Auto update from push.bat

--- JAVA-SCRIPT/Javascript Assignment 08/app.js
+++ JAVA-SCRIPT/Javascript Assignment 08/app.js
@@ -1,12 +1,18 @@
-let now = new Date();
-document.write("Current Date & Time: " + now);
+// let now = new Date();
+// document.write("Current Date & Time: " + now);
 
 
-let months = [
-  "January", "February", "March", "April",
-  "May", "June", "July", "August",
-  "September", "October", "November", "December"
-];
-let now = new Date();
-let currentMonth = months[now.getMonth()];
-alert("Current Month: " + currentMonth);
+// let months = [
+//   "January", "February", "March", "April",
+//   "May", "June", "July", "August",
+//   "September", "October", "November", "December"
+// ];
+// let now = new Date();
+// let currentMonth = months[now.getMonth()];
+// alert("Current Month: " + currentMonth);
+
+
+let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+let today = new Date();
+let currentDay = days[today.getDay()];
+alert("Today is: " + currentDay);
