121d02b3aa2f855e02bedaa2da5bd74347e4ad4c
KaleemShahG <shahkaleem269@gmail.com>
2026-01-04 11:05:54 +0500

    Auto update from push.bat

--- JAVA-SCRIPT/Javascript Assignment 09/app.js
+++ JAVA-SCRIPT/Javascript Assignment 09/app.js
@@ -44,3 +44,16 @@
 // let result = square(5);
 // document.write("Square is: " + result);
 
+
+// function factorial(num) {
+//   let result = 1;
+//   for (let i = 1; i <= num; i++) {
+//     result = result * i;
+//   }
+//   return result;
+// }
+// let number = parseInt(prompt("Enter a number"));
+// let fact = factorial(number);
+// document.write("Factorial of " + number + " is: " + fact);
+
+
