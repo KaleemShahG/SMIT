f523259ddb787bb8037d5f66d7365b03e534fa7c
KaleemShahG <shahkaleem269@gmail.com>
2026-01-04 11:00:01 +0500

    Auto update from push.bat

---
+++ JAVA-SCRIPT/Javascript Assignment 09/app.js
@@ -0,0 +1,11 @@
+function showDateTime() {
+  let now = new Date();
+  document.write("Current Date & Time: " + now);
+}
+showDateTime();
+
+
+function greetUser(firstName, lastName) {
+  document.write("Hello " + firstName + " " + lastName);
+}
+greetUser("Syed", "Kaleem");
