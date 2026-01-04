6e209961944f3cc854db51d6ffc2ca3967162106
KaleemShahG <shahkaleem269@gmail.com>
2026-01-04 11:04:13 +0500

    Auto update from push.bat

--- JAVA-SCRIPT/Javascript Assignment 09/app.js
+++ JAVA-SCRIPT/Javascript Assignment 09/app.js
@@ -1,11 +1,46 @@
-function showDateTime() {
-  let now = new Date();
-  document.write("Current Date & Time: " + now);
-}
-showDateTime();
+// function showDateTime() {
+//   let now = new Date();
+//   document.write("Current Date & Time: " + now);
+// }
+// showDateTime();
 
 
-function greetUser(firstName, lastName) {
-  document.write("Hello " + firstName + " " + lastName);
-}
-greetUser("Syed", "Kaleem");
+// function greetUser(firstName, lastName) {
+//   document.write("Hello " + firstName + " " + lastName);
+// }
+// greetUser("Syed", "Kaleem");
+
+
+// function addNumbers(num1, num2) {
+//   return num1 + num2;
+// }
+// let number1 = parseFloat(prompt("Enter first number"));
+// let number2 = parseFloat(prompt("Enter second number"));
+// let result = addNumbers(number1, number2);
+// document.write("Sum of two numbers is: " + result);
+// function calculate(num1, num2, operator) {
+//   if (operator === "+") {
+//     return num1 + num2;
+//   } else if (operator === "-") {
+//     return num1 - num2;
+//   } else if (operator === "*") {
+//     return num1 * num2;
+//   } else if (operator === "/") {
+//     return num1 / num2;
+//   } else {
+//     return "Invalid Operator";
+//   }
+// }
+// let n1 = parseFloat(prompt("Enter first number"));
+// let n2 = parseFloat(prompt("Enter second number"));
+// let op = prompt("Enter operator (+, -, *, /)");
+// let result = calculate(n1, n2, op);
+// document.write("Result: " + result);
+
+
+// function square(num) {
+//   return num * num;
+// }
+// let result = square(5);
+// document.write("Square is: " + result);
+
