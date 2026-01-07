4a867308d05def864fa0f45c838d4227defc45ba
KaleemShahG <shahkaleem269@gmail.com>
2026-01-07 23:37:59 +0500

    Auto update from push.bat

--- JAVA-SCRIPT/Javascript Assignment 09/demo.js
+++ JAVA-SCRIPT/Javascript Assignment 09/demo.js
@@ -8,3 +8,11 @@
 // let number = parseInt(prompt("Enter a number"));
 // let fact = factorial(number);
 // document.write("Factorial of " + number + " is: " + fact);
+
+// function showCounting(start, end) {
+//   for (let i = start; i <= end; i++) {
+//     document.write(i + "<br>"); }
+// }
+// let startNum = parseInt(prompt("Enter start number"));
+// let endNum = parseInt(prompt("Enter end number"));
+// showCounting(startNum, endNum);
