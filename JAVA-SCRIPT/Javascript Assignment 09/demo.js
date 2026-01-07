5ed3b4fa07ed2533f4269d642c5baa06dab30f31
KaleemShahG <shahkaleem269@gmail.com>
2026-01-07 23:37:29 +0500

    Auto update from push.bat

---
+++ JAVA-SCRIPT/Javascript Assignment 09/demo.js
@@ -0,0 +1,10 @@
+// function factorial(num) {
+//   let result = 1;
+//   for (let i = 1; i <= num; i++) {
+//     result = result * i;
+//   }
+//   return result;
+// }
+// let number = parseInt(prompt("Enter a number"));
+// let fact = factorial(number);
+// document.write("Factorial of " + number + " is: " + fact);
