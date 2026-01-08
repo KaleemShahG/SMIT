f73e4076faa330bcc2da62fa974ac4947671b7f7
KaleemShahG <shahkaleem269@gmail.com>
2026-01-08 21:09:15 +0500

    Auto update from push.bat

---
+++ JAVA-SCRIPT/class 9/extras.js
@@ -0,0 +1,16 @@
+function calculation(){
+    const num1 = 12;
+    const num2 = 6;
+    const operator = "*";
+    if (operator === "+") {
+        console.log(num1 + num2);
+    } else if (operator === "-") {
+        console.log(num1 - num2);
+    } else if (operator === "*") {
+        console.log(num1 * num2);
+    } else if (operator === "/") {
+            console.log(num1 / num2);
+        }
+    
+}
+calculation();
