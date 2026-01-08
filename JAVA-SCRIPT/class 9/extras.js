a648e90154f1790aa0ca4ba200a23aa35df320c1
KaleemShahG <shahkaleem269@gmail.com>
2026-01-08 21:09:33 +0500

    Auto update from push.bat

--- JAVA-SCRIPT/class 9/extras.js
+++ JAVA-SCRIPT/class 9/extras.js
@@ -14,3 +14,17 @@ function calculation(){
     
 }
 calculation();
+
+
+//FUNCTIONS WITH PARAMETERS AND ARGUMENTS
+// console.log(hours+ ":" +minutes+":"+seconds);
+// console.log( date +":"+ month +":"+ year )
+// function greetUser(bisma , Shayan ){
+//     console.log("how are u ");
+//     console.log("get lost");
+//     console.log("give me more money");
+// }
+// greetUser();
+
+// var username = prompt("Enter Your Name")
+// console.log(username);
