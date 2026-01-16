5abda9a98fc60ed0be2be70bd3b9f9eb1248ed91
KaleemShahG <shahkaleem269@gmail.com>
2026-01-16 10:33:59 +0500

    Auto update from push.bat

--- JAVA-SCRIPT/class 11/app.js
+++ JAVA-SCRIPT/class 11/app.js
@@ -1,8 +1,10 @@
 function getInputValue(){
     var Input = document.getElementById("username")
     var InputValue = Input.value.trim()
-    console.log(Input.Value);
-
-    
-
+    console.log(InputValue);
+if(InputValue === ""){
+        alert("Please enter a username");
+    }else{
+        alert("Username is valid");
+    }
 }
