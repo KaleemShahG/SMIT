27474062f3036c9d741b0899e2c6e6e652daa4e0
KaleemShahG <shahkaleem269@gmail.com>
2026-01-18 00:04:40 +0500

    Auto update from push.bat

--- JAVA-SCRIPT/Javascript Assigiment 11/app.js
+++ JAVA-SCRIPT/Javascript Assigiment 11/app.js
@@ -6,3 +6,23 @@ function mobileAlert(mobileName) {
     let row = btn.parentNode.parentNode;
     row.remove();
   }
+
+  function changeImage(img) {
+    img.src = "image2.jpg";
+  }
+
+  function resetImage(img) {
+    img.src = "image1.jpg";
+  }
+
+   let counter = 0;
+
+  function increase() {
+    counter++;
+    document.getElementById("count").innerText = counter;
+  }
+
+  function decrease() {
+    counter--;
+    document.getElementById("count").innerText = counter;
+  }
